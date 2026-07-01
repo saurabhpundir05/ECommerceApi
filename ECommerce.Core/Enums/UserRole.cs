09e3e87e750178272d3304d67a97ac2ea15f057a
Saurabh Pundir <saurabh.pundir05@gmail.com>
2026-07-02 00:27:45 +0530

    feat : add user entity and add reference in project

---
+++ ECommerce.Core/Enums/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Core.Enums
+{
+    public enum UserRole
+    {
+        Admin,
+        User,
+        Seller
+    }
+}
